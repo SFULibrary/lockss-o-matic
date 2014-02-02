3aec60366b65f981b09e0401087dff85d89ceb54
Mark Jordan <mjordan@sfu.ca>
2014-02-02 09:16:11 -0800

    Added Symfony Standard Edition 2.4.1.

---
+++ vendor/symfony/symfony/src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/form_widget_simple.html.php
@@ -0,0 +1,1 @@
+<input type="<?php echo isset($type) ? $view->escape($type) : 'text' ?>" <?php echo $view['form']->block($form, 'widget_attributes') ?><?php if (!empty($value)): ?> value="<?php echo $view->escape($value) ?>"<?php endif ?> />
