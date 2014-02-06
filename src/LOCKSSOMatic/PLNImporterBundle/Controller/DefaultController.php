dc6dd3b78723e3333a63ec63f88733fa99f86a32
Marcus Emmanuel Barnes <mbarnes@sfu.ca>
2014-02-06 15:26:12 -0800

    Basic routing example for further development.

--- src/LOCKSSOMatic/PLNImporterBundle/Controller/DefaultController.php
+++ src/LOCKSSOMatic/PLNImporterBundle/Controller/DefaultController.php
@@ -3,11 +3,15 @@
 namespace LOCKSSOMatic\PLNImporterBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+#use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('LOCKSSOMaticPLNImporterBundle:Default:index.html.twig', array('name' => $name));
+        $response = $this->render('LOCKSSOMaticPLNImporterBundle:Default:index.html.twig', array());
+        $response->headers->set('Content-Type', 'text/html');
+        return $response;
     }
+    
 }
