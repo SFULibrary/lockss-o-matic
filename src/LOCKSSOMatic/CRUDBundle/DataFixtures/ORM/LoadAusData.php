ae9d9207f5b81078752d5991882bfd603304d8f3
Mark Jordan <mjordan@sfu.ca>
2014-02-10 07:30:22 -0800

    Added fixtures for loading a sample Content Provider and a sample AU.

---
+++ src/LOCKSSOMatic/CRUDBundle/DataFixtures/ORM/LoadAusData.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace LOCKSSOMatic\CRUDBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use LOCKSSOMatic\CRUDBundle\Entity\Aus;
+
+class LoadAusData implements FixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $au = new Aus();
+        $au->setPlnsId('1');
+        $au->setAuid('TestAuName');
+        $au->setName('Test application');
+        $au->setManifestUrl('http://foo.example.com/manifest.htm');
+
+        $manager->persist($au);
+        $manager->flush();
+    }
+}
