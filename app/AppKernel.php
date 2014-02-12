7884de52b02406b8747e4b7cda21f0ac5bb6c8a6
Mark Jordan <mjordan@sfu.ca>
2014-02-11 17:33:25 -0800

    Removed vendors.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            // new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new LOCKSSOMatic\SWORDBundle\LOCKSSOMaticSWORDBundle(),
             new LOCKSSOMatic\CRUDBundle\LOCKSSOMaticCRUDBundle(),
