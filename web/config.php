e4663a0bd7001bb66601e11094e889a3d90c4463
Mark Jordan <mjordan@sfu.ca>
2014-02-17 12:09:34 -0800

    Modified web/config.php.

--- web/config.php
+++ web/config.php
@@ -40,20 +40,7 @@ $minorProblems = $symfonyRequirements->getFailedRecommendations();
                 <div class="search">
                   <form method="get" action="http://symfony.com/search">
                     <div class="form-row">
-
-                      <label for="search-id">
-                          <img src="bundles/framework/images/grey_magnifier.png" alt="Search on Symfony website" />
-                      </label>
-
-                      <input name="q" id="search-id" type="search" placeholder="Search on Symfony website" />
-
-                      <button type="submit" class="sf-button">
-                          <span class="border-l">
-                            <span class="border-r">
-                                <span class="btn-bg">OK</span>
-                            </span>
-                        </span>
-                      </button>
+                    <p></p>
                     </div>
                    </form>
                 </div>
@@ -63,10 +50,9 @@ $minorProblems = $symfonyRequirements->getFailedRecommendations();
                 <div class="block">
                     <div class="symfony-block-content">
                         <h1 class="title">Welcome!</h1>
-                        <p>Welcome to your new Symfony project.</p>
+                        <p>Welcome to LOCKSS-O-Matic.</p>
                         <p>
-                            This script will guide you through the basic configuration of your project.
-                            You can also do the same by editing the ‘<strong>app/config/parameters.yml</strong>’ file directly.
+                            This script checks you system to make sure it meets all requirements.
                         </p>
 
                         <?php if (count($majorProblems)): ?>
@@ -107,10 +93,6 @@ $minorProblems = $symfonyRequirements->getFailedRecommendations();
                         <?php endif; ?>
 
                         <ul class="symfony-install-continue">
-                            <?php if (!count($majorProblems)): ?>
-                                <li><a href="app_dev.php/_configurator/">Configure your Symfony Application online</a></li>
-                                <li><a href="app_dev.php/">Bypass configuration and go to the Welcome page</a></li>
-                            <?php endif; ?>
                             <?php if (count($majorProblems) || count($minorProblems)): ?>
                                 <li><a href="config.php">Re-check configuration</a></li>
                             <?php endif; ?>
