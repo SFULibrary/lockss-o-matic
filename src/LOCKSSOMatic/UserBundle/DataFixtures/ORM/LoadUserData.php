b6c5a31b9d892a45aefcf339802dfccbd9333511
Michael Joyce <ubermichael@gmail.com>
2014-10-28 14:09:02 -0700

    Only load one user as a fixture. Now that the tests all create their own user, there's no need for all the extra users.

--- src/LOCKSSOMatic/UserBundle/DataFixtures/ORM/LoadUserData.php
+++ src/LOCKSSOMatic/UserBundle/DataFixtures/ORM/LoadUserData.php
@@ -36,25 +36,6 @@ use LOCKSSOMatic\UserBundle\Entity\User;
  */
 class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
 {
-
-    /**
-     * Users to create.
-     *
-     * @var array
-     */
-    private $userdata = array(
-        array('franklin-admin', 'franklin.admin@example.com', 'franklin Admin', 'ROLE_LOMADMIN'),
-        array('franklin-depositor', 'franklin.depositor@example.com', 'franklin depositor', null),
-        array('franklin-monitor', 'franklin.monitor@example.com', 'franklin monitor', null),
-        array('dewey-admin', 'dewey.admin@example.com', 'dewey Admin', 'ROLE_LOMADMIN'),
-        array('dewey-depositor', 'dewey.depositor@example.com', 'dewey depositor', 'ROLE_LOMADMIN'),
-        array('dewey-monitor', 'dewey.monitor@example.com', 'dewey monitor', null),
-        array('shared-admin', 'shared.admin@example.com', 'shared Admin', 'ROLE_LOMADMIN'),
-        array('shared-depositor', 'shared.depositor@example.com', 'shared depositor', null),
-        array('shared-monitor', 'shared.monitor@example.com', 'shared monitor', null),
-        array('user-noaccess', 'user@example.com', 'User without access', null),
-    );
-
     /**
      * Load the user fixtures.
      *
@@ -62,46 +43,19 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
      */
     public function load(ObjectManager $manager)
     {
-        $admin = $this->buildUser($manager, 'admin@example.com', 'Site admin', 'ROLE_ADMIN');
-        $this->setReference('admin-user', $admin);
-        
-        foreach ($this->userdata as $d) {
-            $user = $this->buildUser($manager, $d[1], $d[2], $d[3]);
-            $this->setReference($d[0], $user);
-        }
-
+        $admin = new User();
+        $admin->setUsername('admin@example.com');
+        $admin->setEmail('admin@example.com');
+        $admin->setFullname('Admin user');
+        $admin->setInstitution('Admin institution');
+        $admin->setEnabled(true);
+        $admin->setLocked(false);
+        $admin->addRole('ROLE_ADMIN');
+        $admin->setPlainPassword('supersecret');
+        $manager->persist($admin);        
         $manager->flush();
     }
-
-    /**
-     * Build a new user and return it.
-     *
-     * @param string $username the user name
-     * @param string $fullname the full name
-     * @param string $role     the role, as remembered in LoadRoleData
-     *
-     * @return User
-     */
-    private function buildUser($manager, $username, $fullname, $role = null)
-    {
-        $user = new User();
-        // AdminUserType sets the username to the email address, so do that here as well.
-        $user->setUsername($username);
-        $user->setEmail($username);
-        $user->setFullname($fullname);
-        $user->setInstitution("");
-        $user->setEnabled(true);
-        $user->setLocked(false);
-        
-        if ($role !== null) {
-            $user->addRole($role);
-        }
-        $user->setPlainPassword('supersecret');
-        $manager->persist($user);
-
-        return $user;
-    }
-
+    
     /**
      * Users must be loaded before ACLs.
      *
