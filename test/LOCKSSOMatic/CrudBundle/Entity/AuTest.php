b49094b35c482c0704901f7c2fa026a21e33161f
Michael Joyce <ubermichael@gmail.com>
2016-05-10 09:36:27 -0700

    start reorganizing the tests and fixing them.

---
+++ test/LOCKSSOMatic/CrudBundle/Entity/AuTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace LOCKSSOMatic\CrudBundle\Entity;
+
+use LOCKSSOMatic\CoreBundle\Utilities\AbstractTestCase;
+
+class AuTest extends AbstractTestCase
+{
+
+    protected $au;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->au = new Au();
+    }
+
+    public function fixtures()
+    {
+        return array(
+            'LOCKSSOMatic\CrudBundle\DataFixtures\ORM\test\LoadAu',
+        );
+    }
+
+    public function testDefaults()
+    {
+        $this->assertInstanceOf('LOCKSSOMatic\CrudBundle\Entity\Au', $this->au);
+        $this->assertCount(0, $this->au->getAuProperties());
+        $this->assertCount(0, $this->au->getAuStatus());
+        $this->assertCount(0, $this->au->getContent());
+    }
+
+    public function testGetRootPluginProperties()
+    {
+        $props = $this->references->getReference('au')->getRootPluginProperties();
+        $this->assertCount(1, $props);
+        $this->assertEquals('root', $props[0]->getPropertyKey());
+    }
+
+    public function testGetAuPropertyValue()
+    {
+        $au = $this->references->getReference('au');
+        $this->assertEquals('2007', $au->getAuPropertyValue('year'));
+    }
+
+    public function testGetAuPropertyValueEncoded()
+    {
+        $au = $this->references->getReference('au');
+        $this->assertEquals('http%3A%2F%2Fexample%2Ecom', $au->getAuPropertyValue('base_url', true));
+    }
+
+    public function testGetAuPropertyValueNull()
+    {
+        $au = $this->references->getReference('au');
+        $this->assertEquals('', $au->getAuPropertyValue('cheeses', true));
+    }
+
+    public function testGetAuProperty()
+    {
+        $au = $this->references->getReference('au');
+        $prop = $au->getAuProperty('year');
+        $this->assertEquals('param.2', $prop->getPropertyKey());
+    }
+
+    public function testGetAuPropertyNull()
+    {
+        $au = $this->references->getReference('au');
+        $prop = $au->getAuProperty('cheeses');
+        $this->assertEquals(null, $prop);
+    }    
+}
---
+++ web/js/f528204.js
@@ -0,0 +1,34 @@
+(function($){
+    var $addLink = $("<a href='#' class='addItem btn btn-primary'><span class='glyphicon glyphicon-plus'></span> Add</span>");
+    
+    function addValueForm($container, count) {
+        var prototype = $container.data('prototype');
+        var index = $container.data('count');
+        var $form = $(prototype.replace(/__name__/g, index).replace(/label__/g, ''));
+        $form.find('label').append(" <a href='#' class='delItem'>Remove</a>");
+        $container.append($form);
+        $('.delItem').click(function(e){
+            e.preventDefault();
+            $(this).closest('div').remove();
+        });
+        $container.data('count', index+1);
+    }
+    
+    $(document).ready(function(){
+        var $container = $('div[data-prototype]');
+        $container.after($addLink);
+        $container.data('count', $container.find('div.form-group').length);
+        $addLink.click(function(e){
+            e.preventDefault();
+            addValueForm($container);
+        });
+        $container.find('label').each(function(){
+            var $this = $(this);
+            $this.append(" <a href='#' class='delItem'>Remove</a>");            
+        });
+        $('.delItem').click(function(e){
+            e.preventDefault();
+            $(this).closest('div').remove();
+        });
+    });
+})(jQuery);
