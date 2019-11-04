8d2422490a2d59379e1b5e9d878341feec51658a
Michael Joyce <ubermichael@gmail.com>
2019-11-04 10:20:15 -0800

    cleaning up a bit.

--- sami.php
+++ sami.php
@@ -1,21 +1,23 @@
 <?php
 
-require 'vendor/autoload.php';
-
 use Sami\Sami;
 use Sami\RemoteRepository\GitHubRemoteRepository;
 use Symfony\Component\Finder\Finder;
 
+$dir = __DIR__;
+
+$config = array(
+//    'theme'                => 'symfony',
+    'title'                => 'Circus Internal API',
+    'build_dir'            => $dir . '/web/docs/api',
+    'cache_dir'            => $dir . '/var/cache/sami',
+    'remote_repository'    => new GitHubRemoteRepository('ubermichael/circus', $dir),
+    'default_opened_level' => 2,
+);
+
 $iterator = Finder::create()
     ->files()
     ->name('*.php')
-    ->exclude('Resources')
-    ->exclude('Tests')
-    ->in(__DIR__ . '/src');
+    ->in('src');
 
-return new Sami($iterator, array(
-    'title' => 'LOCKSSOMatic API',
-    'build_dir' => __DIR__ . '/docs/api',
-    'cache_dir' => __DIR__ . '/app/cache/sami',
-    'remote_repository' => new GitHubRemoteRepository('ubermichael/lockss-o-matic', __DIR__),    
-));
+return new Sami($iterator, $config);
